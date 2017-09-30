745bd1d62b7fd652b1c7e9f82743c775b13dcbdd
Vikram Gopali <vikram@capitalquotient.com>
2017-10-01 03:25:57 +0530

    enabled the route

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,16 @@ var sqlWrapper = require('../database/sqlWrapper');
 /* GET home page. */
 router.post('/register', function(req, res, next) {
   console.log("inside register route",req.body);
-  sqlWrapper.executeQuery(["select * from records"]).then((result)=>{
+  var name = req.body.name;
+  var email = req.body.email;
+  var mobile = req.body.mobile;
+  var bloodGroup = req.body.bloodGroup;
+  var address = req.body.address;
+
+  var query = "insert into records (name, email, mobile, bloodgroup, address, last_donated) values ('"+ name +"','"+ email +"','"+ mobile +"','"+ bloodGroup +"','"+ address +"')";
+  console.log(query)
+
+  sqlWrapper.executeQuery([query]).then((result)=>{
     console.log("res is ",result)
   });
   res.json({"status":true});
