641ee019e5e610636d336e308410dd11ac65334a
Vikram Gopali <vikram@capitalquotient.com>
2017-10-01 02:34:46 +0530

    initial commit

---
+++ database/dbConfig.js
@@ -0,0 +1,11 @@
+//connection properties
+module.exports = {
+    'connection' : {
+        connectionLimit : 30, //important
+        host     : "172.31.28.99", //Deployment : 172.31.61.172
+        user     : "appuser", //Deployment : appuser
+        password : "manchali@123", //Deployment : ruIPkXcUEPZ3lBmg
+        database : "blood", //Deployment : beta_capitalQuotient
+        debug    :  false
+    }
+};
