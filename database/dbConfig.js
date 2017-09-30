7f96dbb9cbe02f82153bd26658e691ce3c004862
Vikram Gopali <vikram@capitalquotient.com>
2017-10-01 03:12:39 +0530

    edit db config

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,9 +2,9 @@
 module.exports = {
     'connection' : {
         connectionLimit : 30, //important
-        host     : "172.31.28.99", //Deployment : 172.31.61.172
-        user     : "appuser", //Deployment : appuser
-        password : "manchali@123", //Deployment : ruIPkXcUEPZ3lBmg
+        host     : "localhost", //Deployment : 172.31.61.172
+        user     : "root", //Deployment : appuser
+        password : "root", //Deployment : ruIPkXcUEPZ3lBmg
         database : "blood", //Deployment : beta_capitalQuotient
         debug    :  false
     }
