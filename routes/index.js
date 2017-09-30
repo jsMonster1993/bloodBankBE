fd45af69a827ac8c861fbf9ebfae7440d42d1343
Vikram Gopali <vikram@capitalquotient.com>
2017-10-01 03:07:34 +0530

    checking DB connection

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var sqlWrapper = require('../database/sqlWrapper');
 
 /* GET home page. */
 router.post('/register', function(req, res, next) {
+  console.log("inside register route",req.body);
+  sqlWrapper.executeQuery(["select * from records"]).then((result)=>{
+    console.log("res is ",result)
+  });
   res.json({"status":true});
 });
 
